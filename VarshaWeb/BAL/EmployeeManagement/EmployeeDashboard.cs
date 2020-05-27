29329f38a14e54baec53f37efc8f5d94c3207f10
VARSHA247-ascenwork <varsha@ascenwork.com>
2020-05-27 19:33:36 +0530

    ok

--- VarshaWeb/BAL/EmployeeManagement/EmployeeDashboard.cs
+++ VarshaWeb/BAL/EmployeeManagement/EmployeeDashboard.cs
@@ -38,29 +38,21 @@ namespace VarshaWeb.BAL.EmployeeManagement
             {
                 EmpBasicinfo.Add(new Emp_BasicInfoModels
                 {
-                    ID = Convert.ToInt32(j["ID"]),
+                         //  ID= Convert.ToInt32(j["ID"]),
+                  
                     FirstName = j["FirstName"].ToString(),
                     MiddleName = j["MiddleName"].ToString(),
                     LastName = j["LastName"].ToString(),
+                    Company = j["Company"]["CompanyName"].ToString(),
+                    // EmpCode = j["EmpCode"].ToString(),
                     EmpCode = j["EmpCode"].ToString(),
-                    JoiningDate = Convert.ToString(j["JoiningDate"]).Trim(),
-                    DOB = Convert.ToString(j["DOB"]).Trim(),
-                    Gender = j["Gender"].ToString(),
-                    MaritalStatus = j["MartialStatus"].ToString(),
-                    OnProbationTill = j["ClientCity"].ToString(),
-                    ProbationStatus = j["ClientRemark"].ToString(),
-                    Manager = j["Manager"].ToString(),
-                    OfficeEmail= j["OfficeEmail"].ToString(),
-                    ContactNumber = j["ContactNumber"].ToString(),
-                    EmpStatus = j["EmpStatus"].ToString(),
-                    Company = j["Company"].ToString(),
-                    Designation = j["Designation"].ToString(),
+                    Designation = j["Designation"]["Designation"].ToString(),
                     Department = j["Department"].ToString(),
-                    Division = j["Designation"].ToString(),
-                    Region = j["Region"].ToString(),
-                    Branch = j["Branch"].ToString(),
-                    User_Name = j["User_Name"].ToString(),
-                    Manager_Code=j["Manager_Code"].ToString()
+                    //  Department = j["Department"].ToString(),
+
+
+
+
                 });
             }
 
@@ -80,8 +72,8 @@ namespace VarshaWeb.BAL.EmployeeManagement
                 JArray jArray = new JArray();
                 RESTOption rESTOption = new RESTOption();
                 rESTOption.filter = filter;
-            rESTOption.select = "ID,FirstName,MiddleName,LastName,EmpCode,Gender,MaritalStatus,DOB,JoiningDate,OnProbationTill,ProbationStatus,OfficeEmail,ContactNumber,EmpStatus,Designation/Id,Designation/Designation,Department/Id,Department/DepartmentName,Division/Id,Division/Division,Region/Id,Region/Region,Branch/Id,Branch/Branch,Manager/Id,Manager/Manager_Code,Company/Id,Company/CompanyName,User_Name";
-            rESTOption.expand = "Company,Designation,Manager,Department,Division,Region,Branch";
+            rESTOption.select = "ID,FirstName,MiddleName,LastName,EmpCode,Gender,MaritalStatus,DOB,JoiningDate,OnProbationTill,ProbationStatus,OfficeEmail,ContactNumber,EmpStatus,Designation/Id,Designation/Designation,Department/Id,Department/DepartmentName,Division/Id,Division/Division,Region/Id,Region/Region,Branch/Id,Branch/Branch,Company/Id,Company/CompanyName,Manager/Id,User_Name/Id,User_Name/Title";
+            rESTOption.expand = "Company,Designation,Department,Division,Region,Branch,User_Name,Manager";
             rESTOption.top = "5000";
 
 
--- VarshaWeb/Controllers/EmployeeManagement/EmployeeDashboardController.cs
+++ VarshaWeb/Controllers/EmployeeManagement/EmployeeDashboardController.cs
@@ -6,16 +6,13 @@ using System.Web.Mvc;
 using VarshaWeb.BAL.EmployeeManagement;
 using VarshaWeb.Models.EmployeeManagement;
 
+
 namespace VarshaWeb.Controllers.EmployeeManagement
 {
     public class EmployeeDashboardController : Controller
     {
         // GET: EmployeeDashboard
         public ActionResult Index()
-        {
-            return View();
-        }
-        public ActionResult EmployeeDashboard()
         {
             List<Emp_BasicInfoModels> empBasicinfo = new List<Emp_BasicInfoModels>();
             var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext);
@@ -26,6 +23,11 @@ namespace VarshaWeb.Controllers.EmployeeManagement
 
             }
             ViewBag.EmpList = empBasicinfo;
+            return View();
+        }
+        public ActionResult EmployeeDashboard()
+        {
+            
             return View();
         }
     }
